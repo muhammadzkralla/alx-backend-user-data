15fb48dddb80cafa07cfa9ee01d675ae272a83e2
Zkrallah <muhammad.heshamyt@gmail.com>
2024-08-06 23:31:35 +0300

    feat: Decoding Base64 Basic auth from header.

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,7 @@
 """ Basic auth
 """
 
+import base64
 from api.v1.auth.auth import Auth
 
 
@@ -19,3 +20,14 @@ class BasicAuth(Auth):
         if not h.startswith("Basic "):
             return None
         return h.split(" ", 1)[1]
+
+    def decode_base64_authorization_header(self, h: str) -> str:
+        """Decodes the Base64 authorization header"""
+        if not isinstance(h, str):
+            return None
+        try:
+            base64_bytes = h.encode('utf-8')
+            decoded_bytes = base64.b64decode(base64_bytes)
+            return decoded_bytes.decode('utf-8')
+        except (base64.binascii.Error, UnicodeDecodeError):
+            return None
