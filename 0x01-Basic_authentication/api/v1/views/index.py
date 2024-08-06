17419f731f9f2655f008fad01bd5cc5a51210942
Zkrallah <muhammad.heshamyt@gmail.com>
2024-08-06 22:41:38 +0300

    feat: Forbidden requests handled.

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -33,3 +33,12 @@ def unauthorized() -> str:
       - Unauthorized response
     """
     abort(401)
+
+
+@app_views.route('/forbidden/', strict_slashes=False)
+def forbidden() -> str:
+    """ GET /api/v1/dorbidden
+    Return:
+      - Forbidden response
+    """
+    abort(403)
