99d87912f64bfa4897f073641497f69a9c96a22e
Zkrallah <muhammad.heshamyt@gmail.com>
2024-08-06 23:42:09 +0300

    feat: Returning user from email and password.

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,6 +4,8 @@
 
 import base64
 from api.v1.auth.auth import Auth
+from models.user import User
+from typing import TypeVar
 
 
 class BasicAuth(Auth):
@@ -40,3 +42,17 @@ class BasicAuth(Auth):
         if ':' not in h:
             return None, None
         return h.split(":", 1)
+
+    def user_object_from_credentials(self, em: str, pd: str) -> TypeVar('User'):
+        """ Returns the User instance based on
+          email and password. """
+        if not isinstance(em, str) or not isinstance(pd, str):
+            return None
+
+        user = User.search(em)
+        if not user:
+            return None
+        user = user[0]  # Assuming search returns a list of User instances
+        if not user.is_valid_password(pd):
+            return None
+        return user
