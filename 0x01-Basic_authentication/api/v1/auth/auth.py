cb7c7f422d884d1344facf4ad19bba2a50452b08
Zkrallah <muhammad.heshamyt@gmail.com>
2024-08-06 23:05:10 +0300

    feat: Implemented the require_auth and the authorization_header functions.

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -10,12 +10,23 @@ class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ require_auth impl
         """
-        return False
+        if not path or not excluded_paths or len(excluded_paths) == 0:
+            return True
+
+        if path[-1] != '/':
+            path = path + '/'
+
+        for excluded_path in excluded_paths:
+            if path == excluded_path:
+                return False
+
+        return True
 
     def authorization_header(self, request=None) -> str:
         """ authorization_header impl
         """
-        return None
+        if request:
+            return request.headers.get("Authorization")
 
     def current_user(self, request=None) -> TypeVar('User'):
         """ current_user impl
