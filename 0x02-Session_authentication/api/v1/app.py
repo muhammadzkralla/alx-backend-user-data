d352cf64d705c0d038560f5e30813c1bd7056602
Zkrallah <muhammad.heshamyt@gmail.com>
2024-08-09 20:18:09 +0300

    feat: Checking session cookie before request.

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -34,12 +34,17 @@ def before_request():
 
     # Paths that do not require authentication
     excluded_paths = ['/api/v1/status/',
-                      '/api/v1/unauthorized/', '/api/v1/forbidden/']
+                      '/api/v1/unauthorized/', '/api/v1/forbidden/',
+                      '/api/v1/auth_session/login/']
+
+    if auth.authorization_header(request):
+        return None
 
     if not auth.require_auth(request.path, excluded_paths):
         return
 
-    if auth.authorization_header(request) is None:
+    if auth.authorization_header(request) is None or \
+            auth.session_cookie(request):
         abort(401)
 
     request.current_user = auth.current_user(request)
