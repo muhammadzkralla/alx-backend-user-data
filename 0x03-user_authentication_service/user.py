54529040c39590c6647767f472d00bab35e4f4a2
Zkrallah <muhammad.heshamyt@gmail.com>
2024-08-13 14:30:31 +0300

    feat: User table and db init.

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -18,3 +18,9 @@ class User(Base):
     hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250), nullable=True)
     reset_token = Column(String(250), nullable=True)
+
+    def __repr__(self):
+        """
+        String representation.
+        """
+        return f"User: id={self.id}"
