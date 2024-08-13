01f45c32a266210aafa9783e6f4783b12d3dc6d1
Zkrallah <muhammad.heshamyt@gmail.com>
2024-08-13 14:44:53 +0300

    feat: Find user by any passed field.

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,7 +5,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
 from user import Base, User
 
 
@@ -32,7 +33,7 @@ class DB:
 
     def add_user(self, email: str, hashed_password: str) -> User:
         """
-        Add user to the database.
+        add_user
         """
         if not email or not hashed_password:
             return
@@ -40,4 +41,20 @@ class DB:
         session = self._session
         session.add(user)
         session.commit()
+        return user
+
+    def find_user_by(self, **kwargs) -> User:
+        """
+        find_user_by
+        """
+        if not kwargs:
+            raise InvalidRequestError("No arguments provided")
+
+        try:
+            user = self._session.query(User).filter_by(**kwargs).one()
+        except NoResultFound:
+            raise NoResultFound("No user found for the given parameters")
+        except InvalidRequestError:
+            raise InvalidRequestError("Invalid parameters provided")
+
         return user
