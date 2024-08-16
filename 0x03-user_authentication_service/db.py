1cec5979bcf9ed88145ce990082bb9098ade9b10
Zkrallah <muhammad.heshamyt@gmail.com>
2024-08-17 02:04:27 +0300

    0x03 Done

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,30 +1,37 @@
 #!/usr/bin/env python3
-"""DB module
+""" DB module
 """
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm.session import Session
 from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.orm.exc import NoResultFound
 from user import Base, User
+from typing import TypeVar
+
+VALID_FIELDS = ['id', 'email', 'hashed_password', 'session_id',
+                'reset_token']
 
 
 class DB:
-    """DB class
+    """
+    DB class.
     """
 
-    def __init__(self) -> None:
-        """Initialize a new DB instance
+    def __init__(self):
         """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        Constructor.
+        """
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
 
     @property
-    def _session(self) -> Session:
-        """Memoized session object
+    def _session(self):
+        """
+        _session.
         """
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
@@ -33,7 +40,7 @@ class DB:
 
     def add_user(self, email: str, hashed_password: str) -> User:
         """
-        add_user
+        add_user.
         """
         if not email or not hashed_password:
             return
@@ -45,16 +52,24 @@ class DB:
 
     def find_user_by(self, **kwargs) -> User:
         """
-        find_user_by
+        find_user_by.
         """
-        if not kwargs:
-            raise InvalidRequestError("No arguments provided")
-
+        if not kwargs or any(x not in VALID_FIELDS for x in kwargs):
+            raise InvalidRequestError
+        session = self._session
         try:
-            user = self._session.query(User).filter_by(**kwargs).one()
-        except NoResultFound:
-            raise NoResultFound("No user found for the given parameters")
-        except InvalidRequestError:
-            raise InvalidRequestError("Invalid parameters provided")
+            return session.query(User).filter_by(**kwargs).one()
+        except Exception:
+            raise NoResultFound
 
-        return user
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """
+        update_user.
+        """
+        session = self._session
+        user = self.find_user_by(id=user_id)
+        for k, v in kwargs.items():
+            if k not in VALID_FIELDS:
+                raise ValueError
+            setattr(user, k, v)
+        session.commit()
